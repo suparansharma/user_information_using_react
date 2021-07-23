e2d2a232018950863759230e7df8d7394b9acba8
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-24 02:32:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [friends,setFriends]=useState([]);
+  useEffect(() =>{
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res =>res.json())
+    .then(data=>setFriends(data))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+   <h1>The number of users are : {friends.length}</h1>
     </div>
   );
 }
