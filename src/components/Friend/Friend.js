48f3f9d30a1baaf6e0e9f126bcd086a6f4df243f
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-25 01:58:05 +0600

    show details

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
     const {name,email,id}=props.friend;
-
+ 
+    const history = useHistory();
+    const handleClick = (friendId)=>{
+    const url = `/friend/${friendId}`;
+        history.push(url)
+    }
+ 
     const friendStyle = {
         border : '1px solid purple',
         margin : '20px',
@@ -15,6 +21,7 @@ const Friend = (props) => {
             <h1>Name ={name}</h1>
             <h2>Email = {email}</h2>
             <p>ID: <Link to={`/friend/${id}`}>Show Details of {id}</Link> </p>
+            <button onClick={()=>handleClick(id)}>Details</button>
         </div>
     );
 };
--- src/components/FriendDetails/FriendDetails.js
+++ src/components/FriendDetails/FriendDetails.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
     const {friendId} = useParams();
+    const[friend,setFriend] = useState({});
+    useEffect(()=>{
+
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data =>setFriend(data));
+
+    },[])
     return (
         <div>
-           <h1> This is all aboout  {friendId} </h1>
+           <p> This is all about Id No{friendId} </p>
+           <h1>Name :{friend.name} </h1>
+           <p>Email:{friend.email}</p>
+           <p>Phone:{friend.phone}</p>
+           <p>Website:{friend.website}</p>
+
+
         </div>
     );
 };
